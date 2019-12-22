cdf5ce853207100d9f49965e2f31de594400d372
Nilesh D <nileshlund@gmail.com>
2019-12-22 11:31:03 +0530

    Completed December 11

---
+++ December-11/Python_Nilesh2000.py
@@ -0,0 +1,24 @@
+import re
+
+
+def valid_email(email):
+    parts = email.split('@')
+    if len(parts) != 2:
+        return False
+
+    domain = parts[1]
+    if domain[-4:] != ".com":
+        return False
+
+    service = domain[:-4]
+    if not service.isalpha():
+        return False
+
+    if re.match('^[A-za-z0-9._-]*$', parts[0]) == None:
+        return False
+
+    return True
+
+
+print(valid_email('john-doe31@gmail.com'))
+print(valid_email('jane.austen_691@dnarifle.com'))
