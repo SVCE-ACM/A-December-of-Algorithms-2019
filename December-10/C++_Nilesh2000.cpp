c02398fde75d7a47ba7df9463d5073197afa9bac
Nilesh D <nileshlund@gmail.com>
2019-12-22 12:43:50 +0530

    Completed December 10

---
+++ December-10/C++_Nilesh2000.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int cookieCount(int n, int p, int c)
+{
+  int Total, Jars;
+  Total = Jars = n / p;
+  while (Jars >= c)
+  {
+    Total += Jars / c;
+    Jars = Jars / c + Jars % c;
+  }
+  return Total;
+}
+
+int main(void)
+{
+  int n, p, c;
+  cout << "\nEnter n, p and c : ";
+  cin >> n >> p >> c;
+  cout << cookieCount(n, p, c);
+}
