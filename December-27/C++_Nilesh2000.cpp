f884d22e3643b9b273ca0062406dae7c238402c6
Nilesh D <nileshlund@gmail.com>
2019-12-28 22:00:18 +0530

    Completed December 27 Question

---
+++ December-27/C++_Nilesh2000.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+void printSpiral(int Arr[10][10], int m, int n)
+{
+  int rowStart = 0, colStart = 0, matIter;
+  while (rowStart < m && colStart < n)
+  {
+    // Print first row
+    for (matIter = colStart; matIter < n; matIter++)
+    {
+      cout << Arr[rowStart][matIter] << " ";
+    }
+    rowStart++;
+
+    // Print last column
+    for (matIter = rowStart; matIter < m; matIter++)
+    {
+      cout << Arr[matIter][n - 1] << " ";
+    }
+    n--;
+
+    // Print last row
+    if (rowStart < m)
+    {
+      for (matIter = n - 1; matIter >= colStart; matIter--)
+      {
+        cout << Arr[m - 1][matIter] << " ";
+      }
+      m--;
+    }
+
+    // Print first column
+    if (colStart < n)
+    {
+      for (matIter = m - 1; matIter >= rowStart; matIter--)
+      {
+        cout << Arr[matIter][colStart] << " ";
+      }
+      colStart++;
+    }
+  }
+}
+
+int main(void)
+{
+  int Rows, Cols;
+  cout << "\nEnter number of rows and columns : ";
+  cin >> Rows >> Cols;
+  int Arr[10][10];
+  for (int i = 0; i < Rows; i++)
+  {
+    for (int j = 0; j < Cols; j++)
+    {
+      cin >> Arr[i][j];
+    }
+  }
+  printSpiral(Arr, Rows, Cols);
+}
