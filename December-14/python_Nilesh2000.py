ec84538db7a8c9f7b09520819b584dbf4b986cf4
Nilesh D <nileshlund@gmail.com>
2019-12-31 19:00:26 +0530

    Completed December 14 Question

---
+++ December-14/python_Nilesh2000.py
@@ -0,0 +1,18 @@
+def subString(s, n):
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    scoreA = scoreB = 0
+    for i in range(n):
+        for len in range(i+1, n+1):
+            subsStr = s[i: len]
+            if subsStr[0] in vowel:
+                scoreA += 1
+            else:
+                scoreB += 1
+    if scoreA > scoreB:
+        print("The winner is A with", scoreA, "points")
+    else:
+        print("The winner is B with", scoreB, "points.")
+
+
+s = input("Enter string: ")
+subString(s, len(s))
