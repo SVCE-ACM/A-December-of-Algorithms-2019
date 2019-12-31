2384f2b675a947028088706be717834b96dd36e0
Nilesh D <nileshlund@gmail.com>
2020-01-01 02:02:35 +0530

    Solved December 15 Question

---
+++ December-15/C++_Nilesh2000.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void totalQuantity(int n, string Str = "", int a = 0, int b = 0)
+{
+  if (a == b && a == n)
+  {
+    cout << Str << " ";
+  }
+  if (a <= n)
+  {
+    totalQuantity(n, Str + "A", a + 1, b);
+    if (b < a)
+    {
+      totalQuantity(n, Str + "B", a, b + 1);
+    }
+  }
+}
+
+int main(void)
+{
+  int quantityOfA;
+  cout << "\nEnter the quantity of A : ";
+  cin >> quantityOfA;
+  totalQuantity(quantityOfA);
+}
